ff44bc1c280db0041e1ae5ab31798f95adb99cdf
ethan <ethxnoo9@gmail.com>
2025-09-09 15:58:35 -0400

    change starting point of i from 0 to 1 in Multiples

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -2,7 +2,8 @@ package lab1;
 
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz.doFizzBuzz(5);
-        FizzBuzz.main(new String[]{});
+//        FizzBuzz.doFizzBuzz(5);
+//        FizzBuzz.main(new String[]{});
+        Multiples.multiples(10, 3, 5);
     }
 }
--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -9,7 +9,7 @@ public class Multiples {
     public static int multiples(int n, int a, int b) {
         int count = 0;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
 
             boolean divisibleBy3 = i % a == 0;
             boolean divisibleBy5 = i % b == 0;
