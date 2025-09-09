c062e91bb77ff88ec9211d454a440ad83513a17e
ethan <ethxnoo9@gmail.com>
2025-09-09 15:21:06 -0400

    run fizzbuzz on main.java and introduced multiples.java

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,24 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    private static int multiples(int n, int a, int b) {
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            boolean divisibleBy3 = i % a == 0;
+            boolean divisibleBy5 = i % b == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        return count;
+    }
+}
